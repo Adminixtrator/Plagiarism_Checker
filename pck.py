fa0360ea68a0e27e3a3046bf590b8a9f0df30501
Adminixtrator <sadiqheritage927@gmail.com>
2019-12-11 09:51:33 +0100

    Update pck.py
    Updated

--- pck.py
+++ pck.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, redirect, url_for, requests
 from urlextract import URLExtract
 
-extractor = URLExtractor
+extractor = URLExtract()
 
 app = Flask(__name__, template_folder = './')
 @app.route('/', methods=['GET','POST'])
 def homepage():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
