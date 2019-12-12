6813e1be9a5c09ef853e74703a6b7a1ba2c4ec82
Adminixtrator <sadiqheritage927@gmail.com>
2019-12-12 16:56:12 +0100

    Update pck.py
    Preparing ground

--- pck.py
+++ pck.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, requests
+from flask import Flask, render_template, redirect, url_for
 from urlextract import URLExtract
 
 extractor = URLExtract()
